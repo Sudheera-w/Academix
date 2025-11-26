f021a9e261281221e9f18c5a390ee6db1e319a23
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-11-27 00:08:34 +0530

    Add student entity

---
+++ backend/backend/src/main/java/com/academix/backend/entity/Student.java
@@ -0,0 +1,24 @@
+package com.academix.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY) //only include id in responses, not expect it in requests
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+    private int year;
+}
