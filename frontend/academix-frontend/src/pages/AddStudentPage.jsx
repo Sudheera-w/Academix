51589197f59067c219182fd7696731ec6c82e66e
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-12-04 21:22:43 +0530

    Set up initial StudentsPage and AddStudentPage for routing structure

---
+++ frontend/academix-frontend/src/pages/AddStudentPage.jsx
@@ -0,0 +1,9 @@
+function AddStudentPage(){
+    return (
+        <div>
+            <h1>Add Student Page</h1>
+        </div>
+    )
+}
+
+export default AddStudentPage;
--- frontend/academix-frontend/src/router/AppRouter.jsx
+++ frontend/academix-frontend/src/router/AppRouter.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
+import StudentsPage from "../pages/StudentsPage.jsx";
+import AddStudentPage from "../pages/AddStudentPage.jsx";
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/Students" element={<StudentsPage/>}/>
+                <Route path="/Students/add" element={<AddStudentPage />} />
             </Routes>
         </BrowserRouter>
     );
