ae256dbec783b645d2418328c7c3bf2c4f8277c2
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-12-04 19:11:39 +0530

    Initial frontend files

---
+++ frontend/academix-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to AcademiX</h1>
+            <p>Student Enrollment Management System</p>
+        </div>
+    );
+}
+
+export default HomePage;
