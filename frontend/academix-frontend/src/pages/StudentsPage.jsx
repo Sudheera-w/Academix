04476e4e9c21e80607e53394921d92d5af32e6ce
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-12-05 23:06:41 +0530

    feat: implement StudentsPage with table rendering and API integration

--- frontend/academix-frontend/src/pages/StudentsPage.jsx
+++ frontend/academix-frontend/src/pages/StudentsPage.jsx
@@ -1,9 +1,48 @@
+import {useEffect,useState} from "react";
+import StudentService from "../Services/StudentService.jsx";
+
 function StudentsPage(){
+
+    const [students,setStudents]= useState([])
+
+    useEffect(()=>{
+        StudentService.getAllStudents().then((response)=>{setStudents(response.data);})
+        .catch((error)=>{console.error("Error fetching students:",error)});
+    },[])
     return(
         <div>
-        <h1>Students Page</h1>
+        <h1>Students</h1>
+            <table
+            border="2"
+            cellPadding="10"
+            style={{
+                width:"100%",
+                borderCollapse:"collapse",
+                textAlign:"center",
+                background:"#2852bc",
+                marginTop:"20px",
+            }}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {students.map((student)=>(
+                    <tr key = {student.id}>
+                        <td>{student.name}</td>
+                        <td>{student.email}</td>
+                        <td>{student.department}</td>
+                        <td>{student.year}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
 
-export default StudentsPage
+export default StudentsPage;
